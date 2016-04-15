9861c99074682c6edbf1eab084248bceebdc165f
Virgílio Santos <virgilio.santos@gmail.com>
2016-04-15 10:02:54 -0300

    update elements, new version

--- estadao.js
+++ estadao.js
@@ -1,33 +1,19 @@
-Element.prototype.remove = function() {
-    this.parentElement.removeChild(this);
-}
-
-NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
-    for(var i = 0, len = this.length; i < len; i++) {
-        if(this[i] && this[i].parentElement) {
-            this[i].parentElement.removeChild(this[i]);
-        }
-    }
-}
-
-
 jQuery(document).ready(function(){
-    var mask = document.getElementById("pw-mask");
-    var content = document.getElementById("pw-content");
-    var page = document.body.parentNode;
-    
-    function freeWilly(){
-        mask.remove();
-        content.remove();
-        jQuery("html").css({
-            "overflow"  : "auto",
-            "overflowX" : "auto",
-            "overflowY" : "auto"
+
+    (function (){
+        var toremove = [
+            document.getElementById("signwall"),
+            document.getElementById("pw-mask"),
+            document.getElementById("pw-content")];
+
+        toremove.forEach(function(e){
+            if(e) e.parentNode.removeChild(e);
         });
         console.log("Willy is fucking free ;)");
-    }
+    })();
+
+    document.body.parentNode.style = "";
     
-    freeWilly();
 });
 
 
